7391689c1b1eb2baa6846ad09b7755092e019e13
Angel Escalera <agescaleraaguirre@mail.fhsu.edu>
2024-10-03 20:13:03 -0500

    Created the requirements.txt and started our initial python file called Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+### INF601 - Advanced Programming in Python
+### Angel Escalera
+### Mini Project 2
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+laptops = pd.read_csv("laptop_prices.csv", index_col=0)
+print(laptops.head())
