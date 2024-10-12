ab9f1d832debaaad6f60d18931c49fe6823450f8
Angel Escalera <agescaleraaguirre@mail.fhsu.edu>
2024-10-12 18:48:15 -0500

    Installed plotly.express.

--- Main.py
+++ Main.py
@@ -4,13 +4,29 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+for dirname, _, filenames in os.walk('laptop_prices.csv'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+import plotly.express as px
 
 laptops = pd.read_csv("laptop_prices.csv", index_col=0)
 
 laptops["Prices_euros"] = laptops["Price_euros"]
 print(laptops.head())
 
-axs = laptops.plot.area(figsize=(12, 4), subplots=True)
+#axs = laptops.plot.area(figsize=(12, 4), subplots=True)
 
 
+df = pd.read_csv('laptop_prices.csv')
+df.head()
+df.info
+df.describe()
+df.isnull().info()
 
+fig = px.scatter(df,x='Company',y='Price_euros',color='Company')
+fig.show()
